8c852d8a7b32df092134aa71281bd51ee4ece621
pierre818181 <pierre.bastola@runpod.io>
2024-09-26 16:57:13 -0500

    update input syntax

--- handler.py
+++ handler.py
@@ -7,7 +7,7 @@ def build_image(job):
     context = job_input["context"]
     dockerfile_path = job_input["dockerfile_path"]
     destination = job_input["destination"]
-    print("context", context, "dockerfile_path", dockerfile_path, "destination", destination)
-    subprocess.run(["/kaniko/executor", "--context", context, "--dockerfile", dockerfile_path, "--destination", destination])
+    print(["/kaniko/executor", "--context", context, "--dockerfile", dockerfile_path, "--destination", destination])
+    subprocess.run(["/kaniko/executor", "--context={}".format(context), "--dockerfile={}".format(dockerfile_path), "--destination={}".format(destination)])
 
 runpod.serverless.start({"handler": build_image})

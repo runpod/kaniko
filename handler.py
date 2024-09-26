24b55e629f2e088c4144658d931aa41be1013c8d
pierre818181 <pierre.bastola@runpod.io>
2024-09-26 16:06:11 -0500

    added python handler

---
+++ handler.py
@@ -0,0 +1,13 @@
+import runpod
+import subprocess
+
+def build_image(job):
+
+    job_input = job["input"]
+    context = job_input["context"]
+    dockerfile_path = job_input["dockerfile_path"]
+    destination = job_input["destination"]
+
+    subprocess.run(["/kaniko/executor", "--context", context, "--dockerfile", dockerfile_path, "--destination", destination])
+
+runpod.serverless.start({"handler": build_image})

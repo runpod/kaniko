23ed33faa925919c179315f718dd654117d2b90e
pierre818181 <pierre.bastola@runpod.io>
2024-09-30 17:05:21 -0500

    add /kaniko

--- handler.py
+++ handler.py
@@ -8,7 +8,7 @@ def build_image(job):
     destination = job_input["destination"]
 
     subprocess.run(["/kaniko/executor", "--context={}".format(context), "--dockerfile={}".format(dockerfile_path), "--destination={}".format(destination), "--no-push", "--tarPath=/runpod-volume/image.tar"])
-    subprocess.run(["bun", "install"], cwd="serverless-registry/push")
-    subprocess.run(["TAR_PATH=/runpod-volume/image.tar", "echo", "Innovator81@", "|", "USERNAME_REGISTRY=pierre", "bun", "run", "index.ts", "r2-registry-production.pierre-bastola.workers.dev/runpod8:latest"], cwd="serverless-registry/push")
+    subprocess.run(["bun", "install"], cwd="/kaniko/serverless-registry/push")
+    subprocess.run(["TAR_PATH=/runpod-volume/image.tar", "echo", "Innovator81@", "|", "USERNAME_REGISTRY=pierre", "bun", "run", "index.ts", "r2-registry-production.pierre-bastola.workers.dev/runpod8:latest"], cwd="/kaniko/serverless-registry/push")
 
 runpod.serverless.start({"handler": build_image})

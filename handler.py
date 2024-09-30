b9cbfa8db9cce3ec79972106005f7273921224b2
pierre818181 <pierre.bastola@runpod.io>
2024-09-30 17:42:36 -0500

    add unzip

--- handler.py
+++ handler.py
@@ -8,7 +8,7 @@ def build_image(job):
     destination = job_input["destination"]
 
     subprocess.run(["mv", "-p", "/kaniko", "/runpod-volume/kaniko"])
-    subprocess.run(["/kaniko/executor", "--context={}".format(context), "--dockerfile={}".format(dockerfile_path), "--destination={}".format(destination), "--no-push", "--tarPath=/runpod-volume/image.tar"])
+    subprocess.run(["/runpod-volume/kaniko/executor", "--context={}".format(context), "--dockerfile={}".format(dockerfile_path), "--destination={}".format(destination), "--no-push", "--tarPath=/runpod-volume/image.tar"])
     subprocess.run(["bun", "install"], cwd="/runpod-volume/kaniko/serverless-registry/push")
     subprocess.run(["TAR_PATH=/runpod-volume/image.tar", "echo", "Innovator81@", "|", "USERNAME_REGISTRY=pierre", "bun", "run", "index.ts", "r2-registry-production.pierre-bastola.workers.dev/runpod8:latest"], cwd="/runpod-volume/kaniko/serverless-registry/push")
 

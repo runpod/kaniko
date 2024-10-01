cb63b5456f105fb21c061f0bcd12a44240785c5f
pierre818181 <pierre.bastola@runpod.io>
2024-09-30 21:06:57 -0500

    download bun runtime

--- handler.py
+++ handler.py
@@ -7,11 +7,12 @@ def build_image(job):
     dockerfile_path = job_input["dockerfile_path"]
     destination = job_input["destination"]
     
+    subprocess.run(["curl", "-fsSL", "https://bun.sh/install", "|", "bash"])
     subprocess.run(["/kaniko/executor", "--context={}".format(context), "--dockerfile={}".format(dockerfile_path), "--destination={}".format(destination), "--no-push", "--tarPath=/runpod-volume/image.tar"])
     subprocess.run(["bun", "install"], cwd="/kaniko/serverless-registry/push")
     subprocess.run(["TAR_PATH=/runpod-volume/image.tar", "echo", "Innovator81@", "|", "USERNAME_REGISTRY=pierre", "bun", "run", "index.ts", "r2-registry-production.pierre-bastola.workers.dev/runpod8:latest"], cwd="/kaniko/serverless-registry/push")
 
-
+    return True
 
     # subprocess.run(["/kaniko/executor", "--context={}".format(context), "--dockerfile={}".format(dockerfile_path), "--destination={}".format(destination), "--no-push", "--tarPath=/runpod-volume/image.tar"])
     # subprocess.run(["bun", "install"], cwd="serverless-registry/push")

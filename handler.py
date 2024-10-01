36325f1acc8de1c46751171565a9ebde02536263
pierre818181 <pierre.bastola@runpod.io>
2024-10-01 09:48:39 -0500

    differeent bun command

--- handler.py
+++ handler.py
@@ -1,18 +1,27 @@
 import runpod
 import subprocess
+import os
 
 def build_image(job):
     job_input = job["input"]
     context = job_input["context"]
     dockerfile_path = job_input["dockerfile_path"]
     destination = job_input["destination"]
-    
-    subprocess.run("curl -fsSL https://bun.sh/install | bash", shell=True, executable="/bin/bash")
-    subprocess.run(["/kaniko/executor", "--context={}".format(context), "--dockerfile={}".format(dockerfile_path), "--destination={}".format(destination), "--no-push", "--tarPath=/runpod-volume/image.tar"])
-    envs = { "USERNAME_REGISTRY": "pierre-bastola", "TAR_PATH": "/runpod-volume/image.tar" }
-    subprocess.run("curl -fsSL https://bun.sh/install | bash && source /root/.bashrc && bun install && echo Innovator81@ | source /root/.bashrc && bun run index.ts r2-registry-production.pierre-bastola.workers.dev/runpod8:latest", cwd="/kaniko/serverless-registry/push", env=envs, shell=True, executable="/bin/bash")
 
-    # subprocess.run("echo Innovator81@ | source /root/.bashrc && bun run index.ts r2-registry-production.pierre-bastola.workers.dev/runpod8:latest", cwd="/kaniko/serverless-registry/push", shell=True, env=envs, executable="/bin/bash")
+    envs = os.environ.copy()
+    install_command = "curl -fsSL https://bun.sh/install | bash"
+    subprocess.run(install_command, shell=True, executable="/bin/bash", env=envs)
+
+    bun_bin_dir = os.path.expanduser("~/.bun/bin")
+    envs["PATH"] = f"{bun_bin_dir}:{envs['PATH']}"
+
+    subprocess.run(["/kaniko/executor", "--context={}".format(context), "--dockerfile={}".format(dockerfile_path), "--destination={}".format(destination), "--no-push", "--tarPath=/runpod-volume/image.tar"])
+    envs["USERNAME_REGISTRY"] = "pierre-bastola"
+    envs["TAR_PATH"] = "/runpod-volume/image.tar"
+
+    subprocess.run("bun install", cwd="/kaniko/serverless-registry/push", env=envs, shell=True, executable="/bin/bash")
+    run_command = "bun run index.ts r2-registry-production.pierre-bastola.workers.dev/runpod8:latest"
+    subprocess.run(run_command, cwd="/kaniko/serverless-registry/push", env=envs, shell=True, executable="/bin/bash")
 
     return True
 

8f291c0763d76711eee673f7c7b66969a7a20cb7
pierre818181 <pierre.bastola@runpod.io>
2024-10-01 09:34:54 -0500

    executable /bin/bash

--- handler.py
+++ handler.py
@@ -9,10 +9,10 @@ def build_image(job):
     
     subprocess.run("curl -fsSL https://bun.sh/install | bash", shell=True, executable="/bin/bash")
     subprocess.run(["/kaniko/executor", "--context={}".format(context), "--dockerfile={}".format(dockerfile_path), "--destination={}".format(destination), "--no-push", "--tarPath=/runpod-volume/image.tar"])
-    subprocess.run("source /root/.bashrc && bun install", cwd="/kaniko/serverless-registry/push", shell=True, executable="/bin/bash")
-
     envs = { "USERNAME_REGISTRY": "pierre-bastola", "TAR_PATH": "/runpod-volume/image.tar" }
-    subprocess.run("echo Innovator81@ | source /root/.bashrc && bun run index.ts r2-registry-production.pierre-bastola.workers.dev/runpod8:latest", cwd="/kaniko/serverless-registry/push", shell=True, env=envs, executable="/bin/bash")
+    subprocess.run("curl -fsSL https://bun.sh/install | bash && source /root/.bashrc && bun install && echo Innovator81@ | source /root/.bashrc && bun run index.ts r2-registry-production.pierre-bastola.workers.dev/runpod8:latest", cwd="/kaniko/serverless-registry/push", env=envs, shell=True, executable="/bin/bash")
+
+    # subprocess.run("echo Innovator81@ | source /root/.bashrc && bun run index.ts r2-registry-production.pierre-bastola.workers.dev/runpod8:latest", cwd="/kaniko/serverless-registry/push", shell=True, env=envs, executable="/bin/bash")
 
     return True
 

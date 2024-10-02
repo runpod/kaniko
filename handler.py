9031ee186213e5f829762220edfbdb7011a4a136
pierre818181 <pierre.bastola@runpod.io>
2024-10-02 16:17:04 -0500

    try --compressed-caching

--- handler.py
+++ handler.py
@@ -19,7 +19,7 @@ def build_image(job):
 
     subprocess.run("mkdir -p /runpod-volume/{}".format(uuid), shell=True, env=envs)
     tarPath = "/runpod-volume/{uuid}/image.tar".format(uuid=uuid)
-    subprocess.run(["/kaniko/executor", "--context={}".format(context), "--dockerfile={}".format(dockerfile_path), "--destination={}".format(destination), "--no-push", "--tar-path={}".format(tarPath)])
+    subprocess.run(["/kaniko/executor", "--context={}".format(context), "--dockerfile={}".format(dockerfile_path), "--destination={}".format(destination), "--no-push", "--use-new-run", "--compressed-caching=false", "--tar-path={}".format(tarPath)])
     envs["USERNAME_REGISTRY"] = "pierre-bastola"
     envs["TAR_PATH"] = tarPath
     envs["UUID"] = uuid

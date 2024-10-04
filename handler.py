e46c44413fc063b3b0df44621c69fdee10555216
pierre818181 <pierre.bastola@runpod.io>
2024-10-04 09:36:59 -0500

    simple handler.py

--- handler.py
+++ handler.py
@@ -4,11 +4,11 @@ import os
 
 def build_image(job):
     job_input = job["input"]
-    context = job_input["context"]
     dockerfile_path = job_input["dockerfile_path"]
     destination = job_input["destination"]
     cloudflare_destination = job_input["cloudflare_destination"]
     uuid = job_input["uuid"]
+    github_repo = job_input["github_repo"]    
 
     envs = os.environ.copy()
     install_command = "curl -fsSL https://bun.sh/install | bash"
@@ -17,11 +17,23 @@ def build_image(job):
     bun_bin_dir = os.path.expanduser("~/.bun/bin")
     envs["PATH"] = f"{bun_bin_dir}:{envs['PATH']}"
 
+    
     subprocess.run("mkdir -p /runpod-volume/{}".format(uuid), shell=True, env=envs)
-    tarPath = "/runpod-volume/{uuid}/image.tar".format(uuid=uuid)
-    subprocess.run(["/kaniko/executor", "--context={}".format(context), "--dockerfile={}".format(dockerfile_path), "--destination={}".format(destination), "--no-push", "--tar-path={}".format(tarPath)])
+
+    contextPath = "/runpod-volume/{}/repo.tar.gz".format(uuid)
+    subprocess.run("git clone {} /runpod-volume/{}/repo".format(github_repo, uuid), shell=True)
+    subprocess.run("tar -czvf {} /runpod-volume/{}/repo".format(contextPath. uuid), shell=True)
+
+    imageBuildPath = "/runpod-volume/{}/image.tar".format(uuid)
+    subprocess.run([
+        "/kaniko/executor", 
+        "--context={}".format(contextPath), 
+        "--dockerfile={}".format(dockerfile_path), 
+        "--destination={}".format(destination), 
+        "--no-push", "--tar-path={}".format(imageBuildPath)]
+    )
     envs["USERNAME_REGISTRY"] = "pierre-bastola"
-    envs["TAR_PATH"] = tarPath
+    envs["TAR_PATH"] = imageBuildPath
     envs["UUID"] = uuid
     
     subprocess.run("bun install", cwd="/kaniko/serverless-registry/push", env=envs, shell=True, executable="/bin/bash")

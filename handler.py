ebe96e88421e459d67c1c389ee4f0e7729f6fbf2
pierre818181 <pierre.bastola@runpod.io>
2024-10-11 11:32:47 -0500

    add error checking to handler.py

--- handler.py
+++ handler.py
@@ -17,10 +17,6 @@ def build_image(job):
     username_registry = job_input["username_registry"]
 
     envs = os.environ.copy()
-    envs["USERNAME_REGISTRY"] = username_registry
-    envs["TAR_PATH"] = imageBuildPath
-    envs["UUID"] = uuid
-    envs["REGISTRY_JWT_TOKEN"] = jwt_token
 
     api_url = f"https://api.github.com/repos/{github_repo.split('/')[-2]}/{github_repo.split('/')[-1]}/tarball/{ref}"
     headers = {
@@ -28,7 +24,6 @@ def build_image(job):
         "Authorization": f"Bearer {auth_token}",
         "X-GitHub-Api-Version": "2022-11-28"
     }
-
     try:
         response = requests.get(api_url, headers=headers, stream=True)
         response.raise_for_status()
@@ -37,7 +32,6 @@ def build_image(job):
 
     temp_dir = f"/runpod-volume/{uuid}/temp"
     os.makedirs(temp_dir, exist_ok=True)
-
     with tarfile.open(fileobj=io.BytesIO(response.content), mode="r:gz") as tar:
         tar.extractall(path=temp_dir)
 
@@ -50,7 +44,6 @@ def build_image(job):
 
     bun_bin_dir = os.path.expanduser("~/.bun/bin")
     envs["PATH"] = f"{bun_bin_dir}:{envs['PATH']}"
-
     repoDir = "/runpod-volume/{}/temp/{}".format(uuid, extracted_dir)
     try:
         subprocess.run("mkdir -p /runpod-volume/{}".format(uuid), shell=True, env=envs, check=True)
@@ -76,6 +69,10 @@ def build_image(job):
     except Exception as e:
         return { "status": "failed", "error": str(e), "refresh_worker": True }
     
+    envs["USERNAME_REGISTRY"] = username_registry
+    envs["TAR_PATH"] = imageBuildPath
+    envs["UUID"] = uuid
+    envs["REGISTRY_JWT_TOKEN"] = jwt_token
     try:
         subprocess.run("bun install", cwd="/kaniko/serverless-registry/push", env=envs, shell=True, executable="/bin/bash")
     except Exception as e:
